1e022ca125ec903df1d05b90b993db0bd4d4b04d
Sravan SS <sravan4863@gmail.com>
2021-03-01 12:17:47 +0530

    Final commit

--- btre/settings.py
+++ btre/settings.py
@@ -138,6 +138,6 @@ MESSAGE_TAGS = {
 # Email config
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'sravan4863@gmail.com'
-EMAIL_HOST_PASSWORD = 'saranga1999'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
